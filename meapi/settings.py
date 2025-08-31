4ab188612e77dfd8bbf5c18bfbac2ec19a8b82f1
Sunny <cse22105@iiitklayani.ac.in>
2025-08-31 14:41:43 +0530

    Simplify database config to use Railway's exact variable names

--- meapi/settings.py
+++ meapi/settings.py
@@ -107,15 +107,15 @@ WSGI_APPLICATION = 'meapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 # Railway provides these MySQL environment variables when you add a MySQL service:
-# MYSQLDATABASE, MYSQLHOST, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER, MYSQL_URL
+# MYSQLDATABASE, MYSQLHOST, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER, MYSQL_URL, MYSQL_ROOT_PASSWORD
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": os.getenv("MYSQLDATABASE", os.getenv("MYSQL_DATABASE", "railway")),
-        "USER": os.getenv("MYSQLUSER", os.getenv("MYSQL_USER", "root")), 
-        "PASSWORD": os.getenv("MYSQLPASSWORD", os.getenv("MYSQL_PASSWORD", "")),
-        "HOST": os.getenv("MYSQLHOST", os.getenv("MYSQL_HOST", "127.0.0.1")),
-        "PORT": os.getenv("MYSQLPORT", os.getenv("MYSQL_PORT", "3306")),
+        "USER": os.getenv("MYSQLUSER", "root"), 
+        "PASSWORD": os.getenv("MYSQLPASSWORD", os.getenv("MYSQL_ROOT_PASSWORD", "")),
+        "HOST": os.getenv("MYSQLHOST", "127.0.0.1"),
+        "PORT": int(os.getenv("MYSQLPORT", "3306")),
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
             "charset": "utf8mb4",
